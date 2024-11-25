8bccd1e86740963524a8c27070216a88e8d26d76
Melrick1 <melrickbatuwael@gmail.com>
2024-11-25 09:52:19 +0800

    Test 4

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 	// define path constants
-    define("APP_PATH", 'http://103.226.139.101/DevOps/public');
+    define("APP_PATH", 'http://www.melrick.online/DevOps/public');
 
     // define database config
     
-    define("HOST_DB", "localhost");
+    define("HOST_DB", "103.226.139.101");
     define("NAME_DB", "db_apotek");
     define("USER_DB", "testadmin");
     define("PASS_DB", "Admin123Mel!");
