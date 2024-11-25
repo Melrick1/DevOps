05b0d0b3f52efb726c00b765080e3abe89908378
Melrick1 <melrickbatuwael@gmail.com>
2024-11-25 09:49:22 +0800

    test3

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@
 
     // define database config
     
-    define("HOST_DB", "103.226.139.101");
+    define("HOST_DB", "localhost");
     define("NAME_DB", "db_apotek");
     define("USER_DB", "testadmin");
     define("PASS_DB", "Admin123Mel!");
